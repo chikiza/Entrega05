6f2056b315969a25954e518aedf9b9f8279aa8ab
chikiza <83772028+chikiza@users.noreply.github.com>
2024-07-21 12:54:45 -0400

    imagenes cargadas
    en app.js

--- entrega05/src/App.js
+++ entrega05/src/App.js
@@ -2,12 +2,13 @@
 import { Component } from "react"
 import Productos from './components/Productos'
 
+
 class App extends Component {
   state = {
     productos: [
-      {name: 'Tomate', price: 1500, img: '/productos/tomate.jpg'},
-      {name: 'Arvejas', price: 2500, img: '/productos/arvejas.jpg'},
-      {name: 'Lechuga', price: 500, img: '/productos/lechuga.jpg'},
+      {name: 'Tomate', price: 1500, img: '/productos/tomate.jpeg'},
+      {name: 'Arvejas', price: 2500, img: '/productos/arbejas.jpeg'},
+      {name: 'Lechuga', price: 500, img: '/productos/lechuga.jpeg'},
     ]
   }
   render() {
